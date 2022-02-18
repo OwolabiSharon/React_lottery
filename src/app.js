2d7659cdc877e393e04a97bfa64d818450a38491
sharexy23 <omotietie@yahoo.com>
2022-02-18 04:55:36 +0100

    no fix yet

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
-import 'normalize.css/normalize.css'
 import './styles/styles.css';
 import AppRouter from './routers/lotteryRouter';
 
